cab870e3c904d64a77076e2127718f2c37bdfd0b
daixuedong <daixuedong06@gmail.com>
2026-05-14 20:55:17 +0800

    寻找好下标

--- goodIndices/goodIndices.hpp
+++ goodIndices/goodIndices.hpp
@@ -4,19 +4,31 @@
 #include <vector>
 class Solution
 {
-    public:
-        std::vector<int> goodIndices(std::vector<int>& nums, int k)
-        {
-            std::vector<int> result = {};
-            int n = nums.size();
-            if (n < k || n-k < 0)
-                return result;
-            for (int i = k; i < n-k; i++)
-            {
-                if (nums[i-k+1] - nums[i-k] != 1 || nums[i+])
-            }
-            return result;
+public:
+    std::vector<int> goodIndices(std::vector<int>& nums, int k)
+    {
+        int n = nums.size();
+        std::vector<int> left(n, 1), right(n, 1), result;
+
+        // 左侧
+        for (int i = 1; i < n; i++) {
+            if (nums[i] <= nums[i-1])
+                left[i] = left[i-1] + 1;
         }
+
+        // 右侧
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] <= nums[i+1])
+                right[i] = right[i+1] + 1;
+        }
+
+        for (int i = k; i < n - k; i++) {
+            if (left[i-1] >= k && right[i+1] >= k) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
 };
 
 #endif
